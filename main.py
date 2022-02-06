9c7cf728b0c57cb2d2a5b6b11d2998f874b6360c
zyusifov02 <zamanyusifov561@gmail.com>
2022-02-06 14:01:43 +0400

    from slow to fast

--- main.py
+++ main.py
@@ -1,41 +1,42 @@
-from flask import Flask, request
 from apps import payment, telegram
+from fastapi import Request, FastAPI
+from fastapi.responses import PlainTextResponse
+from datamodels.telegram_body import TelegramBody
+from datamodels.payment_body import PaymentBody
 
-app = Flask(__name__)
+
+app = FastAPI()
 
 
 # Telegram send requests to this url
-@app.route('/', methods=['POST'])
-def main():
-    obj = request.json
-
-    chat_id = obj["message"]["chat"]["id"]
-    user = obj["message"]["from"]["first_name"]
+@app.post("/")
+def main(obj: TelegramBody):
+    obj = obj.message
+    chat_id = obj["chat"]["id"]
+    user = obj["from"]["first_name"]
     payment_link = payment.get_payment_link(obj)
     text = f"Hello, *{user}*\\!"
 
-    telegram.send_payment_button(chat_id, text, payment_link)
+    r = telegram.send_payment_button(chat_id, text, payment_link)
     return {'ok': True}
 
 
 # Payment service redirect user to this url
-@app.route('/success/')
+@app.get("/success/", response_class=PlainTextResponse)
 def success():
     return "Payment was successful"
 
 
 # Payment service send requests to this url
-@app.route('/confirm_payment/', methods=['POST'])
-def confirm_payment():
+@app.post("/confirm_payment/")
+def confirm_payment(obj: PaymentBody):
     """status, success, meta, chat_id - all this params must be declared in payment service"""
-    obj = request.json
+    obj = obj.data
     chat_id = obj["meta"]["chat_id"]
-    if obj['status']['success']:
+    if obj['status'] == 'success':
         text = "Payment was successful"
-
         telegram.send_message(chat_id, text)
     else:
         text = "Payment was failed"
-
         telegram.send_message(chat_id, text)
     return {'ok': True}
